901e498eca3a42e18f5f30492e272740a5cd4fe1
soer4769 <soer4768@gmail.com>
2023-08-31 11:43:55 +0200

    addde the rest of 1.1

--- assignment-1/ConsoleApp1/Intro2.fs
+++ assignment-1/ConsoleApp1/Intro2.fs
@@ -17,6 +17,7 @@ let cvalue = lookup env "c";;
 type expr = 
   | CstI of int
   | Var of string
+  | If of expr * expr * expr
   | Prim of string * expr * expr;;
 
 let e1 = CstI 17;;
@@ -31,18 +32,25 @@ let e3 = Prim("+", Prim("*", Var "b", CstI 9), Var "a");;
 let rec eval e (env : (string * int) list) : int =
     match e with
     | CstI i                -> i
-    | Var x                 -> lookup env x 
-    | Prim("+", e1, e2)     -> eval e1 env + eval e2 env
+    | Var x                 -> lookup env x
+    | If (e1, e2, e3)       -> if (eval e1 env) <> 0 then eval e2 env else eval e3 env
+    //| Prim("+", e1, e2)     -> eval e1 env + eval e2 env
     | Prim("*", e1, e2)     -> eval e1 env * eval e2 env
     | Prim("-", e1, e2)     -> eval e1 env - eval e2 env
     | Prim("max", e1, e2)   -> max (eval e1 env) (eval e2 env)
     | Prim("min", e1, e2)   -> min (eval e1 env) (eval e2 env)
     | Prim("==", e1, e2)    -> if eval e1 env = eval e2 env then 1 else 0
+    | Prim(ope, e1, e2)     ->
+        let i1 = eval e1 env
+        let i2 = eval e2 env
+        match ope with
+        | "+" -> i1 + i2
 
 let e1v  = eval e1 env;;
 let e2v1 = eval e2 env;;
 let e2v2 = eval e2 [("a", 314)];;
 let e3v  = eval e3 env;;
-let e4v1 = Prim("max", e1v, e2v1)
-let e4v2 = Prim("min", e1v, e2v1)
-let e4v3 = Prim("==", e1v, e2v1)
+let e4v1 = Prim("max", e1, e2)
+let e4v2 = Prim("min", e1, e2)
+let e4v3 = Prim("==", e1, e2)
+let v5   = If(Var "a", CstI 11, CstI 22)
