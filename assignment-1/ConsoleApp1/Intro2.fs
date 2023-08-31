b8b1c6859e4d94919c0972c1ad58746c9e693455
COHL7500 <cohl7500@gmail.com>
2023-08-31 11:17:24 +0200

    make a solution and project

--- assignment-1/ConsoleApp1/Intro2.fs
+++ assignment-1/ConsoleApp1/Intro2.fs
@@ -1,10 +1,4 @@
-(* Programming language concepts for software developers, 2010-08-28 *)
-
-(* Evaluating simple expressions with variables *)
-
-module Intro2
-
-(* Association lists map object language variables to their values *)
+module ConsoleApp1.Intro2
 
 let env = [("a", 3); ("c", 78); ("baf", 666); ("b", 111)];;
 
@@ -38,17 +32,12 @@ let rec eval e (env : (string * int) list) : int =
     match e with
     | CstI i                -> i
     | Var x                 -> lookup env x 
-    // | Prim("+", e1, e2)     -> eval e1 env + eval e2 env
+    | Prim("+", e1, e2)     -> eval e1 env + eval e2 env
     | Prim("*", e1, e2)     -> eval e1 env * eval e2 env
     | Prim("-", e1, e2)     -> eval e1 env - eval e2 env
     | Prim("max", e1, e2)   -> max (eval e1 env) (eval e2 env)
     | Prim("min", e1, e2)   -> min (eval e1 env) (eval e2 env)
     | Prim("==", e1, e2)    -> if eval e1 env = eval e2 env then 1 else 0
-    | Prim(ope, e1, e2)     -> 
-          let i1 = e1  
-          let i2 = e2
-          match ope with
-          | "+" -> i1 + i2
 
 let e1v  = eval e1 env;;
 let e2v1 = eval e2 env;;
