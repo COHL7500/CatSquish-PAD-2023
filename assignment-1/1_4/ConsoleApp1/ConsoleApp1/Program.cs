f4337b75da50b07895ac3388de129198e7ea5acb
ItzWeihao <weihaoandersen@gmail.com>
2023-08-31 15:08:07 +0200

    1.4 (missing part 3)
    1, 2 and 4 is done - missing part 3

--- assignment-1/1_4/ConsoleApp1/ConsoleApp1/Program.cs
+++ assignment-1/1_4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,1 +1,189 @@
-﻿
+﻿namespace ConsoleApp1
+{
+    using System;
+
+    public abstract class Expr
+    {
+    }
+
+    public class CstI : Expr
+    {
+        public int ConstantInt { get; }
+
+        public CstI(int constant)
+        {
+            ConstantInt = constant;
+        }
+    }
+
+    public class Var : Expr
+    {
+        public string Variable { get; }
+
+        public Var(string variable)
+        {
+            Variable = variable;
+        }
+    }
+
+    public class Add : Expr
+    {
+        public Expr e1 { get; }
+        public Expr e2 { get; }
+
+        public Add(Expr expr1, Expr expr2)
+        {
+            e1 = expr1;
+            e2 = expr2;
+        }
+    }
+
+    public class Mul : Expr
+    {
+        public Expr e1 { get; }
+        public Expr e2 { get; }
+
+        public Mul(Expr expr1, Expr expr2)
+        {
+            e1 = expr1;
+            e2 = expr2;
+        }
+    }
+
+    public class Sub : Expr
+    {
+        public Expr e1 { get; }
+        public Expr e2 { get; }
+
+        public Sub(Expr expr1, Expr expr2)
+        {
+            e1 = expr1;
+            e2 = expr2;
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Expr e1 = new Sub(new Var("v"), new Add(new Var("w"), new Var("z")));
+            Expr e2 = new Mul(new CstI(2), e1);
+            Expr e3 = new Add(new Add(new Var("x"), new Var("y")), new Add(new Var("z"), new Var("v")));
+            Expr e4 = new Add(new Var("x"), new CstI(0));
+            Expr e5 = new Mul(new Add(new CstI(1), new CstI(0)), new Add(new Var("x"), new CstI(0)));
+            Expr e6 = new Add(new Mul(new CstI(5), new Var("x")), new CstI(2));
+
+            string fmt(Expr e)
+            {
+                switch (e)
+                {
+                    case CstI c:
+                        return c.ConstantInt.ToString();
+                    case Var v:
+                        return v.Variable;
+                    case Add add:
+                        return $"({fmt(add.e1)} + {fmt(add.e2)})";
+                    case Mul mul:
+                        return $"({fmt(mul.e1)} * {fmt(mul.e2)})";
+                    case Sub sub:
+                        return $"({fmt(sub.e1)} - {fmt(sub.e2)})";
+                    default:
+                        throw new ArgumentException("Unknown expression type");
+                }
+            }
+
+            Expr simplify(Expr e)
+            {
+                switch (e)
+                {
+                    case Add add:
+                        var simpExpAdd1 = simplify(add.e1);
+                        var simpExpAdd2 = simplify(add.e2);
+
+                        if (simpExpAdd1 is CstI ca1 && ca1.ConstantInt == 0)
+                        {
+                            return simpExpAdd2; 
+                        } else if (simpExpAdd2 is CstI ca2 && ca2.ConstantInt == 0)
+                        {
+                            return simpExpAdd1;
+                        } else
+                        {
+                            return new Add(simpExpAdd1, simpExpAdd2); 
+                        }
+                    case Sub sub:
+                        var simpExpSub1 = simplify(sub.e1);
+                        var simpExpSub2 = simplify(sub.e2);
+
+                        if (simpExpSub2 is CstI cs2 && cs2.ConstantInt == 0)
+                        {
+                            return simpExpSub1;
+                        } else if (simpExpSub1 == simpExpSub2)
+                        {
+                            return new CstI(0);
+                        } else
+                        {
+                            return new Sub(simpExpSub1, simpExpSub2);
+                        }
+                    case Mul mul:
+                        var simpExpMul1 = simplify(mul.e1);
+                        var simpExpMul2 = simplify(mul.e2);
+                        
+                        if (simpExpMul1 is CstI cm1 && cm1.ConstantInt == 1)
+                        {
+                            return simpExpMul2;
+                        }
+                        else if (simpExpMul2 is CstI cm2 && cm2.ConstantInt == 1)
+                        {
+                            return simpExpMul1;
+                        }
+                        else if (simpExpMul1 is CstI cm3 && cm3.ConstantInt == 0)
+                        {
+                            return simpExpMul1;
+                        }
+                        else if (simpExpMul2 is CstI cm4 && cm4.ConstantInt == 0)
+                        {
+                            return simpExpMul2;
+                        }
+                        else
+                        {
+                            return new Mul(simpExpMul1, simpExpMul2);
+                        }
+                    default:
+                        return e;
+                }
+            }
+
+            Expr diff(Expr e)
+            {
+                switch (e)
+                {
+                    case CstI:
+                        return new CstI(0);
+                    case Var:
+                        return new CstI(1);
+                    case Add add:
+                        var diffExpAdd1 = diff(add.e1);
+                        var diffExpAdd2 = diff(add.e2);
+                        return simplify(new Add(diffExpAdd1, diffExpAdd2));
+                    case Mul mul:
+                        var diffExpMul1 = diff(mul.e1);
+                        var diffExpMul2 = diff(mul.e2);
+                        return simplify(new Mul(mul.e1, diffExpMul2));
+                    case Sub sub:
+                        var diffExpSub1 = diff(sub.e1);
+                        var diffExpSub2 = diff(sub.e2);
+                        return simplify(new Sub(diffExpSub1, diffExpSub2));
+                    default:
+                        throw new ArgumentException("Unknown expression type");
+                }
+            }
+            
+            Console.WriteLine(fmt(e1));
+            Console.WriteLine(fmt(e2));
+            Console.WriteLine(fmt(e3));
+            Console.WriteLine(fmt(e4));
+            Console.WriteLine(fmt(simplify(e5)));
+            Console.WriteLine(fmt(diff(e6)));
+        }
+    }
+}
--- assignment-1/1_4/ConsoleApp1/ConsoleApp1/Reference.cs
+++ assignment-1/1_4/ConsoleApp1/ConsoleApp1/Reference.cs
@@ -1,214 +1,213 @@
-namespace ConsoleApp1
+
+
+// Object language expressions with variables
+
+public abstract class AExpr
 {
-    using System;
-    
-    // Object language expressions with variables
-    
-    public abstract class AExpr
-    {
-    }
-    
-    public class CstI : AExpr
-    {
-        public int Value { get; }
-        
-        public CstI(int value)
-        {
-            Value = value;
-        }
-    }
-    
-    public class Var : AExpr
-    {
-        public string Name { get; }
-        
-        public Var(string name)
-        {
-            Name = name;
-        }
-    }
-    
-    public class Add : AExpr
-    {
-        public AExpr Expr1 { get; }
-        public AExpr Expr2 { get; }
-        
-        public Add(AExpr expr1, AExpr expr2)
-        {
-            Expr1 = expr1;
-            Expr2 = expr2;
-        }
-    }
-    
-    public class Mul : AExpr
-    {
-        public AExpr Expr1 { get; }
-        public AExpr Expr2 { get; }
-        
-        public Mul(AExpr expr1, AExpr expr2)
-        {
-            Expr1 = expr1;
-            Expr2 = expr2;
-        }
-    }
-    
-    public class Sub : AExpr
-    {
-        public AExpr Expr1 { get; }
-        public AExpr Expr2 { get; }
-        
-        public Sub(AExpr expr1, AExpr expr2)
-        {
-            Expr1 = expr1;
-            Expr2 = expr2;
-        }
-    }
-    
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            AExpr e1 = new Sub(new Var("v"), new Add(new Var("w"), new Var("z")));
-            AExpr e2 = new Mul(new CstI(2), e1);
-            AExpr e3 = new Add(new Add(new Var("x"), new Var("y")), new Add(new Var("z"), new Var("v")));
-            AExpr e4 = new Add(new Var("x"), new CstI(0));
-            AExpr e5 = new Mul(new Add(new CstI(1), new CstI(0)), new Add(new Var("x"), new CstI(0)));
-            AExpr e6 = new Add(new Mul(new CstI(5), new Var("x")), new CstI(2));
+}
 
-            string fmt(AExpr a)
-            {
-                if (a is CstI cstI)
-                {
-                    return cstI.Value.ToString();
-                }
-                else if (a is Var var)
-                {
-                    return var.Name;
-                }
-                else if (a is Add add)
-                {
-                    return $"({fmt(add.Expr1)} + {fmt(add.Expr2)})";
-                }
-                else if (a is Mul mul)
-                {
-                    return $"({fmt(mul.Expr1)} * {fmt(mul.Expr2)})";
-                }
-                else if (a is Sub sub)
-                {
-                    return $"({fmt(sub.Expr1)} - {fmt(sub.Expr2)})";
-                }
-                else
-                {
-                    throw new ArgumentException("Unknown expression type");
-                }
-            }
-
-            AExpr simplify(AExpr a)
-            {
-                if (a is Add add)
-                {
-                    var simplifiedExpr1 = simplify(add.Expr1);
-                    var simplifiedExpr2 = simplify(add.Expr2);
-
-                    if (simplifiedExpr1 is CstI cst1 && cst1.Value == 0)
-                    {
-                        return simplifiedExpr2;
-                    }
-                    else if (simplifiedExpr2 is CstI cst2 && cst2.Value == 0)
-                    {
-                        return simplifiedExpr1;
-                    }
-                    else
-                    {
-                        return new Add(simplifiedExpr1, simplifiedExpr2);
-                    }
-                }
-                else if (a is Sub sub)
-                {
-                    var simplifiedExpr1 = simplify(sub.Expr1);
-                    var simplifiedExpr2 = simplify(sub.Expr2);
-
-                    if (simplifiedExpr2 is CstI cst2 && cst2.Value == 0)
-                    {
-                        return simplifiedExpr1;
-                    }
-                    else if (simplifiedExpr1 == simplifiedExpr2)
-                    {
-                        return new CstI(0);
-                    }
-                    else
-                    {
-                        return new Sub(simplifiedExpr1, simplifiedExpr2);
-                    }
-                }
-                else if (a is Mul mul)
-                {
-                    var simplifiedExpr1 = simplify(mul.Expr1);
-                    var simplifiedExpr2 = simplify(mul.Expr2);
-
-                    if (simplifiedExpr1 is CstI cst1 && cst1.Value == 1)
-                    {
-                        return simplifiedExpr2;
-                    }
-                    else if (simplifiedExpr2 is CstI cst2 && cst2.Value == 1)
-                    {
-                        return simplifiedExpr1;
-                    }
-                    else if (simplifiedExpr1 is CstI cst3 && cst3.Value == 0)
-                    {
-                        return simplifiedExpr1;
-                    }
-                    else if (simplifiedExpr2 is CstI cst4 && cst4.Value == 0)
-                    {
-                        return simplifiedExpr2;
-                    }
-                    else
-                    {
-                        return new Mul(simplifiedExpr1, simplifiedExpr2);
-                    }
-                }
-                else
-                {
-                    return a;
-                }
-            }
-
-            AExpr diff(AExpr a)
-            {
-                if (a is CstI)
-                {
-                    return new CstI(0);
-                }
-                else if (a is Var)
-                {
-                    return new CstI(1);
-                }
-                else if (a is Add add)
-                {
-                    var diffExpr1 = diff(add.Expr1);
-                    var diffExpr2 = diff(add.Expr2);
-                    return simplify(new Add(diffExpr1, diffExpr2));
-                }
-                else if (a is Mul mul)
-                {
-                    var diffExpr1 = diff(mul.Expr1);
-                    var diffExpr2 = diff(mul.Expr2);
-                    return simplify(new Mul(mul.Expr1, diffExpr2));
-                }
-                else if (a is Sub sub)
-                {
-                    var diffExpr1 = diff(sub.Expr1);
-                    var diffExpr2 = diff(sub.Expr2);
-                    return simplify(new Sub(diffExpr1, diffExpr2));
-                }
-                else
-                {
-                    throw new ArgumentException("Unknown expression type");
-                }
-            }
-
-            Console.WriteLine(fmt(simplify(e5)));
-            Console.WriteLine(fmt(diff(e6)));
-        }
+public partial class CstII : AExpr
+{
+    public int Value { get; }
+    
+    public CstII(int value)
+    {
+        Value = value;
     }
 }
+
+public partial class Varr : AExpr
+{
+    public string Name { get; }
+    
+    public Varr(string name)
+    {
+        Name = name;
+    }
+}
+
+public class Addd : AExpr
+{
+    public AExpr Expr1 { get; }
+    public AExpr Expr2 { get; }
+    
+    public Addd(AExpr expr1, AExpr expr2)
+    {
+        Expr1 = expr1;
+        Expr2 = expr2;
+    }
+}
+
+public class Mull : AExpr
+{
+    public AExpr Expr1 { get; }
+    public AExpr Expr2 { get; }
+    
+    public Mull(AExpr expr1, AExpr expr2)
+    {
+        Expr1 = expr1;
+        Expr2 = expr2;
+    }
+}
+
+public class Subb : AExpr
+{
+    public AExpr Expr1 { get; }
+    public AExpr Expr2 { get; }
+    
+    public Subb(AExpr expr1, AExpr expr2)
+    {
+        Expr1 = expr1;
+        Expr2 = expr2;
+    }
+}
+
+/*
+class Programm
+{
+    private static void Main(string[] args)
+    {
+        AExpr e1 = new Subb(new Varr("v"), new Addd(new Varr("w"), new Varr("z")));
+        AExpr e2 = new Mull(new CstII(2), e1);
+        AExpr e3 = new Addd(new Addd(new Varr("x"), new Varr("y")), new Addd(new Varr("z"), new Varr("v")));
+        AExpr e4 = new Addd(new Varr("x"), new CstII(0));
+        AExpr e5 = new Mull(new Addd(new CstII(1), new CstII(0)), new Addd(new Varr("x"), new CstII(0)));
+        AExpr e6 = new Addd(new Mull(new CstII(5), new Varr("x")), new CstII(2));
+
+        string fmt(AExpr a)
+        {
+            if (a is CstII cstI)
+            {
+                return cstI.Value.ToString();
+            }
+            else if (a is Varr var)
+            {
+                return var.Name;
+            }
+            else if (a is Addd add)
+            {
+                return $"({fmt(add.Expr1)} + {fmt(add.Expr2)})";
+            }
+            else if (a is Mull mul)
+            {
+                return $"({fmt(mul.Expr1)} * {fmt(mul.Expr2)})";
+            }
+            else if (a is Subb sub)
+            {
+                return $"({fmt(sub.Expr1)} - {fmt(sub.Expr2)})";
+            }
+            else
+            {
+                throw new ArgumentException("Unknown expression type");
+            }
+        }
+
+        AExpr simplify(AExpr a)
+        {
+            if (a is Addd add)
+            {
+                var simplifiedExpr1 = simplify(add.Expr1);
+                var simplifiedExpr2 = simplify(add.Expr2);
+
+                if (simplifiedExpr1 is CstII cst1 && cst1.Value == 0)
+                {
+                    return simplifiedExpr2;
+                }
+                else if (simplifiedExpr2 is CstII cst2 && cst2.Value == 0)
+                {
+                    return simplifiedExpr1;
+                }
+                else
+                {
+                    return new Addd(simplifiedExpr1, simplifiedExpr2);
+                }
+            }
+            else if (a is Subb sub)
+            {
+                var simplifiedExpr1 = simplify(sub.Expr1);
+                var simplifiedExpr2 = simplify(sub.Expr2);
+
+                if (simplifiedExpr2 is CstII cst2 && cst2.Value == 0)
+                {
+                    return simplifiedExpr1;
+                }
+                else if (simplifiedExpr1 == simplifiedExpr2)
+                {
+                    return new CstII(0);
+                }
+                else
+                {
+                    return new Subb(simplifiedExpr1, simplifiedExpr2);
+                }
+            }
+            else if (a is Mull mul)
+            {
+                var simplifiedExpr1 = simplify(mul.Expr1);
+                var simplifiedExpr2 = simplify(mul.Expr2);
+
+                if (simplifiedExpr1 is CstII cst1 && cst1.Value == 1)
+                {
+                    return simplifiedExpr2;
+                }
+                else if (simplifiedExpr2 is CstII cst2 && cst2.Value == 1)
+                {
+                    return simplifiedExpr1;
+                }
+                else if (simplifiedExpr1 is CstII cst3 && cst3.Value == 0)
+                {
+                    return simplifiedExpr1;
+                }
+                else if (simplifiedExpr2 is CstII cst4 && cst4.Value == 0)
+                {
+                    return simplifiedExpr2;
+                }
+                else
+                {
+                    return new Mull(simplifiedExpr1, simplifiedExpr2);
+                }
+            }
+            else
+            {
+                return a;
+            }
+        }
+
+        AExpr diff(AExpr a)
+        {
+            if (a is CstII)
+            {
+                return new CstII(0);
+            }
+            else if (a is Varr)
+            {
+                return new CstII(1);
+            }
+            else if (a is Addd add)
+            {
+                var diffExpr1 = diff(add.Expr1);
+                var diffExpr2 = diff(add.Expr2);
+                return simplify(new Addd(diffExpr1, diffExpr2));
+            }
+            else if (a is Mull mul)
+            {
+                var diffExpr1 = diff(mul.Expr1);
+                var diffExpr2 = diff(mul.Expr2);
+                return simplify(new Mull(mul.Expr1, diffExpr2));
+            }
+            else if (a is Subb sub)
+            {
+                var diffExpr1 = diff(sub.Expr1);
+                var diffExpr2 = diff(sub.Expr2);
+                return simplify(new Subb(diffExpr1, diffExpr2));
+            }
+            else
+            {
+                throw new ArgumentException("Unknown expression type");
+            }
+        }
+
+        Console.WriteLine(fmt(simplify(e5)));
+        Console.WriteLine(fmt(diff(e6)));
+    }
+}
+*/
