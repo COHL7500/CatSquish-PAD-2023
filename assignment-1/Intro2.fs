67ce725b44bae449bafb5ed7ebb60859bcf66dbb
ItzWeihao <weihaoandersen@gmail.com>
2023-08-31 10:55:02 +0200

    assignment progress

--- assignment-1/Intro2.fs
+++ assignment-1/Intro2.fs
@@ -36,14 +36,19 @@ let e3 = Prim("+", Prim("*", Var "b", CstI 9), Var "a");;
 
 let rec eval e (env : (string * int) list) : int =
     match e with
-    | CstI i            -> i
-    | Var x             -> lookup env x 
-    | Prim("+", e1, e2) -> eval e1 env + eval e2 env
-    | Prim("*", e1, e2) -> eval e1 env * eval e2 env
-    | Prim("-", e1, e2) -> eval e1 env - eval e2 env
-    | Prim _            -> failwith "unknown primitive";;
+    | CstI i                -> i
+    | Var x                 -> lookup env x 
+    | Prim("+", e1, e2)     -> eval e1 env + eval e2 env
+    | Prim("*", e1, e2)     -> eval e1 env * eval e2 env
+    | Prim("-", e1, e2)     -> eval e1 env - eval e2 env
+    | Prim("max", e1, e2)   -> max(eval e1 env) (eval e2 env)
+    | Prim("min", e1, e2)   -> min(eval e1 env) (eval e2 env)
+    | Prim("==", e1, e2)    -> if eval e1 env = eval e2 env then 1 else 0
 
 let e1v  = eval e1 env;;
 let e2v1 = eval e2 env;;
 let e2v2 = eval e2 [("a", 314)];;
 let e3v  = eval e3 env;;
+let e4v1 = Prim("max", eval e1 env, eval e2 env)
+let e4v2 = Prim("min", eval e1 env, eval e2 env)
+let e4v3 = Prim("==", eval e1 env, eval e2 env)
