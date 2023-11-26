c2558dc197433226fc2f5e40659215e5ce7cbcb0
soer4769 <soer4768@gmail.com>
2023-11-26 12:56:54 +0100

    finished exercise 12.4

---
+++ assignment-11/Exercise-12.4/ex28.c
@@ -0,0 +1,13 @@
+// micro-C example 1
+
+void main(int n) {
+  print (true && true);
+  print (true && false);
+  print (false && false);
+  print (false && true);
+
+  print (true || true);
+  print (true || false);
+  print (false || false);
+  print (false || true);
+}
